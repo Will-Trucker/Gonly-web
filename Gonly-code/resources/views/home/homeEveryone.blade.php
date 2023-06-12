ad947d4cc0da7105cf3fdcf9f33de83c0ab7f5a0
Programmer X <diegosaulrojasserrano@gmail.com>
2023-06-11 18:52:22 -0600

    Agregación de laravel UI
    Agregación del plugin para el inicio de registro/inicio de usuario

---
+++ Gonly-code/resources/views/home/homeEveryone.blade.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="icon" type="image/png" href="{{ asset('images/logos/logo-gonly-icon.png') }}">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fredoka:wght@500&display=swap" rel="stylesheet">
+    <script src="https://kit.fontawesome.com/8dd3c39186.js" crossorigin="anonymous"></script>
+    <title>Inicio | Gonly</title>
+
+    @vite(['resources/css/homeEveryone.css'])
+
+</head>
+<body>
+    
+    <header>
+        <nav>
+            <img src="{{ asset('images/logos/logo-gonly-white-letters.png') }}" alt="">
+            <section>
+                <i class="fa-solid fa-magnifying-glass"></i>
+                <form action="">
+                    <input type="text" placeholder="Busta en Gonly" name="" id="">
+                    <input type="submit" value="Buscar">
+                </form>
+                <div>
+                    
+                </div>
+            </section>
+        </nav>
+        <section>
+
+        </section>
+    </header>
+
+</body>
+</html>
