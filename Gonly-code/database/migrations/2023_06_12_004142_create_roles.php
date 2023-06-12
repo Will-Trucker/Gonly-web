e8b54137e6abc421d3499f8ef47980317236249e
William Emmanuel Mazariego Orellana <83888568+Will-Trucker@users.noreply.github.com>
2023-06-12 12:16:04 -0600

    Establecimiento de roles iniciales

--- Gonly-code/database/migrations/2023_06_12_004142_create_roles.php
+++ Gonly-code/database/migrations/2023_06_12_004142_create_roles.php
@@ -3,25 +3,29 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
-
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+use App\Models\User;
 return new class extends Migration
 {
     /**
      * Run the migrations.
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
-        Schema::create('roles', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
+     $role1 = Role::create(['name' => 'admin']);
+     $role2 = Role::create(['name' => 'escritor']);
+     $user = User::find(1);
+     $user->assignRole($role1);
     }
 
     /**
      * Reverse the migrations.
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
-        Schema::dropIfExists('roles');
+
     }
 };
