59c038d5ee0e00e22610dc1f869b8d994c754a12
Gonly Web <juanjo234ortiz@gmail.com>
2023-09-02 15:01:41 -0600

    menu responsive en nav-loged
    falta en nav-guest

--- Gonly/resources/views/layouts/nav-users-loged.blade.php
+++ Gonly/resources/views/layouts/nav-users-loged.blade.php
@@ -1,28 +1,38 @@
 <header>
-    <nav x-data="{ open: false }">
-      <div class="logo-image">
-        <a href="{{ route('welcome') }}"><img class="logo-gonly-black-letters" src="{{ Vite::asset('resources/img/Logos/logo-gonly-black-letters.png') }}"></a>
+<!-- responsive: -->
+<!-- mobile-display-logo, input y label de abajo, off-canva-menu -->
+    <div class="mobile-display-logo">
+      <div class="logo-image2">
+          <a href="{{ route('welcome') }}"><img class="logo-gonly-black-letters" src="{{ Vite::asset('resources/img/Logos/logo-gonly-black-letters.png') }}"></a>
       </div>
-
-      <livewire:search-component/>     
-        
-      <x-translateSwicht />
-
-      <div class="relatve z-10" class="relatve">
-        <button id="menuButton" class="w-52 user-configuration bg-transparent px-2 border-2 border-amber-300 py-0.5">
-          <svg viewBox="0 0 36 36" fill="none" role="img" xmlns="http://www.w3.org/2000/svg" width="80" height="80"><mask id=":r2f:" maskUnits="userSpaceOnUse" x="0" y="0" width="36" height="36"><rect width="36" height="36" rx="72" fill="#FFFFFF"></rect></mask><g mask="url(#:r2f:)"><rect width="36" height="36" fill="#ff005b"></rect><rect x="0" y="0" width="36" height="36" transform="translate(8 8) rotate(214 18 18) scale(1.1)" fill="#ffb238" rx="6"></rect><g transform="translate(4 4) rotate(-4 18 18)"><path d="M13,20 a1,0.75 0 0,0 10,0" fill="#000000"></path><rect x="10" y="14" width="1.5" height="2" rx="1" stroke="none" fill="#000000"></rect><rect x="24" y="14" width="1.5" height="2" rx="1" stroke="none" fill="#000000"></rect></g></g></svg>
-          <h3>{{ Auth::user()->name }}</h3><i class="fa-solid fa-gear"></i>
-        </button>
-        <div id="menuDropdown" class="absolute px-2 bg-white rounded-lg shadow-xl hidden">
-          
-          <form method="POST" action="{{ route('logout') }}">
-            @csrf
-            <a href="{{ route('logout') }}" class="block text-center w-48 py-2 text-gray-800 hover:bg-gray-100 " onclick="event.preventDefault(); this.closest('form').submit();">{{ __('Log Out') }}</a>
-          </form>
-        </div>
-      </div>
-
     </div>
+
+    <input type="checkbox" name="" class="off-canvas-btn">
+    <label class="off-canvas-burger">☰</label>
+
+  <div class="off-canva-menu">
+      <nav x-data="{ open: false }">
+        <div class="logo-image">
+          <a href="{{ route('welcome') }}"><img class="logo-gonly-black-letters" src="{{ Vite::asset('resources/img/Logos/logo-gonly-black-letters.png') }}"></a>
+        </div>
+
+        <livewire:search-component/>     
+          
+        <x-translateSwicht />
+
+        <div class="relatve z-10" class="relatve">
+          <button id="menuButton" class="w-52 user-configuration bg-transparent px-2 border-2 border-amber-300 py-0.5">
+            <svg viewBox="0 0 36 36" fill="none" role="img" xmlns="http://www.w3.org/2000/svg" width="80" height="80"><mask id=":r2f:" maskUnits="userSpaceOnUse" x="0" y="0" width="36" height="36"><rect width="36" height="36" rx="72" fill="#FFFFFF"></rect></mask><g mask="url(#:r2f:)"><rect width="36" height="36" fill="#ff005b"></rect><rect x="0" y="0" width="36" height="36" transform="translate(8 8) rotate(214 18 18) scale(1.1)" fill="#ffb238" rx="6"></rect><g transform="translate(4 4) rotate(-4 18 18)"><path d="M13,20 a1,0.75 0 0,0 10,0" fill="#000000"></path><rect x="10" y="14" width="1.5" height="2" rx="1" stroke="none" fill="#000000"></rect><rect x="24" y="14" width="1.5" height="2" rx="1" stroke="none" fill="#000000"></rect></g></g></svg>
+            <h3>{{ Auth::user()->name }}</h3><i class="fa-solid fa-gear"></i>
+          </button>
+          <div id="menuDropdown" class="absolute px-2 bg-white rounded-lg shadow-xl hidden">
+            
+            <form method="POST" action="{{ route('logout') }}">
+              @csrf
+              <a href="{{ route('logout') }}" class="block text-center w-48 py-2 text-gray-800 hover:bg-gray-100 " onclick="event.preventDefault(); this.closest('form').submit();">{{ __('Log Out') }}</a>
+            </form>
+          </div>
+        </div>
 
     </nav>
     <div class="menu-user-loged">
@@ -34,9 +44,11 @@
         <a href="{{ route('profile.edit') }}" class="link-5"><li>Mi perfil <i class="fa-solid fa-user"></i></li></a>
       </ul>
     </div>
+  </div>
 
 
-    <section class="headers">
+
+    <!-- <section class="headers">
             <br>
             <br>
         <input type="checkbox" class="openSidebarMenu" id="openSidebarMenu">
@@ -54,7 +66,9 @@
             <li><a href="{{ route('profile.edit') }}">Mi perfil</a></li>
             </ul>
         </div>
-        </section>
+  </section> 
+
+-->
 
 
 </header>
