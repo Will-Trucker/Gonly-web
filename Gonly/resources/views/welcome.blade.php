0316203ac7cc5e7180e37efe2ce457a1074e41be
William Emmanuel Mazariego Orellana <83888568+Will-Trucker@users.noreply.github.com>
2023-09-02 09:57:50 -0600

    pruebas

--- Gonly/resources/views/welcome.blade.php
+++ Gonly/resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.head-pretm')
 
 @section('link-css-js')
-    @livewireStyles
+    
     @vite(['resources/css/home/welcome.css', 'resources/css/layouts-css/nav-guest.css', 'resources/css/layouts-css/footer-users.css' , 'resources/js/welcome.js'])
 @endsection
 
@@ -24,11 +24,8 @@
                     <a href="">{{__('Offers of the day')}}</a>
                 </div>
             </div>
-<<<<<<< Updated upstream
             <article class="container-img" >
-=======
             <article class="home-img-cont">
->>>>>>> Stashed changes
                 <img src="{{ Vite::asset('resources/img/Decoration/ilustration-removebg-preview.png') }}" alt="">
             </article>  
         </section>
